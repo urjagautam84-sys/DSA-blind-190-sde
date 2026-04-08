dbcd767f2c957e38c2051d67b6b1839d0474f82b
Urja Gautam <urjagautam@Urjas-MacBook-Air.local>
2026-04-08 22:50:59 +0530

    trees part-2

---
+++ tree-is-balanced.js
@@ -0,0 +1,24 @@
+class Solution {
+    isBalanced(root) {
+        //your code goes here
+        this.dfshieght(root)!=-1;
+        
+    }dfshieght(root){
+        if(root===null){
+            return 0;
+        }
+        let lefthieght= this.dfshieght(root.left);
+        if(lefthieght===-1){
+            return -1;
+        }
+        let righthieght= this.dfshieght(root.right);
+        if(righthieght===-1){
+            return -1;
+        }
+        if(Math.abs(lefthieght-righthieght)>1){
+            return -1;
+        }
+        return Math.max(lefthieght+righthieght)+1;
+
+    }
+}
